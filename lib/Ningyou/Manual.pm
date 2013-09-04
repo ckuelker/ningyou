768624a7fb01967c06479a09710e25daf259543a
Christian Kuelker <christian.kuelker@cipworx.org>
2013-09-04 07:22:00 +0200

    add INSTALLATION

--- lib/Ningyou/Manual.pm
+++ lib/Ningyou/Manual.pm
@@ -10,4 +10,18 @@ __END__
 
 Ningyou::Manual
 
+=head1 INSTALLATION
+
+Use the usual Perl ways to install. For example:
+
+    tar xvzf Ningyou-0.0.1.tar.gz
+    cd Ningyou
+    perl Makefile.PL
+    make
+
+As root
+
+    make install
+
 =cut
+
