d9c946b0d26fff7c71e10a3464d8a4f2719593a1
Christian Kuelker <christian.kuelker@cipworx.org>
2014-01-23 06:01:00 +0100

    add init command

--- lib/Ningyou/Options.pm
+++ lib/Ningyou/Options.pm
@@ -72,7 +72,7 @@ sub process_commands {
     my ( $s, $i ) = @_;
 
     my $x = scalar $i->name;    # remove the object from the input
-    my @x = qw(help show script apply list);
+    my @x = qw(help show script apply list init);
     if ( $x ~~ @x ) {
         $s->set_command($x);
     }
