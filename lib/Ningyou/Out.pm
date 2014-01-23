655d7eee69d7d45d488d9334175a4fe1ea2c028c
Christian Kuelker <christian.kuelker@cipworx.org>
2014-01-23 07:01:00 +0100

    fix: Use of uninitialized value in repeat (x)

--- lib/Ningyou/Out.pm
+++ lib/Ningyou/Out.pm
@@ -7,50 +7,50 @@ use warnings;                    # on by default
 use warnings qw(FATAL utf8);     # make encoding glitches fatal
 use open qw(:std :utf8);         # undeclared streams in UTF-8
 use charnames qw(:full :short);  # unneeded in v5.16
-    use Carp;
+use Carp;
 use Data::Dumper;
 use Moose::Role;
 use namespace::autoclean;
 our $VERSION = '0.0.9';
 use Term::ANSIColor;
 
-# Idea from  Data::Dump::Color 
+# Idea from  Data::Dump::Color
 use vars qw(%COLOR_THEMES %COLORS $COLOR $COLOR_THEME $COLOR_DEPTH);
 %COLOR_THEMES = (
     default16 => {
         colors => {
-	    undef => 'white',
-	    
+            undef => 'white',
+
             version => 'green',
-            host  => 'bright_blue',     
-	    yes   => 'bright_green',
-	    no    => 'bright_red',
-	    mode  => 'blue',
-	    ready => 'green',
-	    execute => 'red',
-	    todo => 'bright_red',
-	    error=> 'bright_red',
-	    done      => 'bright_green',
-            module    => 'yellow',
-	    comment  => 'blue',
-	    command  => 'yellow',
-            file   => 'magenta',
+            host    => 'bright_blue',
+            yes     => 'bright_green',
+            no      => 'bright_red',
+            mode    => 'blue',
+            ready   => 'green',
+            execute => 'red',
+            todo    => 'bright_red',
+            error   => 'bright_red',
+            done    => 'bright_green',
+            module  => 'yellow',
+            comment => 'blue',
+            command => 'yellow',
+            file    => 'magenta',
             module  => 'bright_yellow',
 
-            string  => 'bright_yellow',
-            string  => 'bright_cyan',
-            object  => 'bright_green',
-            symbol  => 'cyan',
-            linum   => 'black on_white', 
+            string => 'bright_yellow',
+            string => 'bright_cyan',
+            object => 'bright_green',
+            symbol => 'cyan',
+            linum  => 'black on_white',
         },
     },
     default256 => {
         color_depth => 256,
         colors      => {
-            version  => 135,
+            version => 135,
             undef   => 124,
-            host  => 27,
-            file   => 51,
+            host    => 27,
+            file    => 51,
             string  => 226,
             object  => 10,
             module  => 10,
@@ -67,16 +67,16 @@ $COLOR_DEPTH = $COLOR_THEMES{$COLOR_THEME}{color_depth} // 16;
 %COLORS = %{ $COLOR_THEMES{$COLOR_THEME}{colors} };
 my $_colreset = color('reset');
 
-sub c { 
+sub c {
     my ( $s, $c, $i ) = @_;
-    my $col = defined $c? $c : 'undef';
+    my $col = defined $c ? $c : 'undef';
     my $colval = $COLORS{$col};
     if ( $COLOR // $ENV{COLOR} // ( -t STDOUT ) ) {
         if ( $COLOR_DEPTH >= 256 && $colval =~ /^\d+$/ ) {
-            return  "\e[38;5;${colval}m" . $i . $_colreset;
+            return "\e[38;5;${colval}m" . $i . $_colreset;
         }
         else {
-            return  color($colval) . $i . $_colreset;
+            return color($colval) . $i . $_colreset;
         }
     }
     else {
@@ -87,9 +87,13 @@ sub c {
 sub o {
     my ( $s, $i ) = @_;
     confess "ERROR: empty string in output" if not defined $i;
+
     #chomp $i;
-    my $o = $s->get_options;
-    my $v = q{ } x $o->{indentation};
+    my $o           = $s->get_options;
+    my $indentation = exists $o->{indentation}
+        and defined $o->{indentation} : $o->{indentation} : 0;
+    my $v = q{ } x $indentation;
+
     #$i = $v . $i . "\n";
     $i = $v . $i;
     if ( exists $o->{debug} and $o->{debug} ) {
@@ -97,7 +101,7 @@ sub o {
         open my $f, q{>>}, $fn or die "Can not open [$fn]";
         print $f $i;
     }
-        print $i if not exists $o->{quite};
+    print $i if not exists $o->{quite};
 }
 1;
 __END__
