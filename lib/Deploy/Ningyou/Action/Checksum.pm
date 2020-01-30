b967d26ffd090d11888bc51e50a707e0372f23c8
Christian Kuelker (w2) <c@c8i.org>
2020-01-30 08:32:44 +0100

    support ningyou URL ningyou://

--- lib/Deploy/Ningyou/Action/Checksum.pm
+++ lib/Deploy/Ningyou/Action/Checksum.pm
@@ -3,9 +3,12 @@
 # |                                                                           |
 # | Provides checksum argument action                                         |
 # |                                                                           |
-# | Version: 0.1.0 (change our $VERSION inside)                               |
+# | Version: 0.1.1 (change our $VERSION inside)                               |
 # |                                                                           |
 # | Changes:                                                                  |
+# |                                                                           |
+# | 0.1.1 2020-01-30 Christian Kuelker <c@c8i.org>                            |
+# |     - support ningyou URL ningyou://                                      |
 # |                                                                           |
 # | 0.1.0 2020-01-04 Christian Kuelker <c@c8i.org>                            |
 # |     - initial release                                                     |
@@ -46,7 +49,7 @@ with qw(
     Deploy::Ningyou::Host
 );
 
-our $VERSION = '0.1.0';
+our $VERSION = '0.1.1';
 
 sub register              { return 'checksum'; }
 sub parameter             { return { module => 0, }; }
@@ -70,7 +73,8 @@ sub apply {
     $s->p("         While this module is quite smart and preserves order\n");
     $s->p("         and comments, it is not bullet prove. It might\n");
     $s->p("         produce invalid configuration. For example it will\n");
-    $s->p("         merge attributes of duplicated sections. Make a backup\n");
+    $s->p(
+        "         merge attributes of duplicated sections. Make a backup\n");
     $s->p("         or commit to git before using this experimental\n");
     $s->p("         command.\n");
     my $updated = 0;
@@ -85,6 +89,8 @@ sub apply {
     my $cwd = qx(pwd);
     chomp $cwd;
     $s->d("cwd[$cwd]\n");
+    my $ok = $s->c( 'yes', 'OK' );
+    my $ng = $s->c( 'no',  'NG' );
     my $fn = "$cwd/$i->{mod}->[0]";
     if ( -f $fn ) {    # do file exist
         $s->d("fn [$fn]\n");
@@ -104,50 +110,45 @@ sub apply {
                 my $cfg = Config::IniFiles->new( -file => $cfn );
                 foreach my $sec ( $cfg->Sections ) {
                     next if not $sec =~ m{file:}gmx;
-                    $s->d("sec [$sec]: ");
-
-                    if (    $cfg->exists( $sec, "source" )
-                        and $cfg->val( $sec, "source" ) eq $fn )
-                    {
-                        $s->d("OK\n");
+                    $s->d("* SECTION sec [$sec]: ");
+                    next if not $cfg->exists( $sec, "source" );
+                    next if not $cfg->exists( $sec, "checksum" );
+                    my $src = $cfg->val( $sec, "source" );
+                    $src =~ s{ningyou://}{$wt/}gmx;
+                    $src =~ s{//}{/}gmx;
+                    if ( $src eq $fn ) {
+                        $s->d("    $ok sec [$sec]\n");
+                        $s->d("    $ok fn  [$fn]\n");
                         $found = 1;
                         push @sec, $sec;
-                        if ( $cfg->exists( $sec, "checksum" ) ) {
 
                         # TODO: consider using Deploy::Ningyou::Util::Provider
                         # my $cs = $s->get_md5(undef,$fn);
                         #print "cs [$cs]\n";
-                            my $ctx = Digest::MD5->new;
-                            open my $f, q{<}, $fn
-                                or die "Can not read [$fn]!\n";
-                            $ctx->addfile($f);
-                            my $digest = $ctx->hexdigest;
-                            close $f;
-                            $s->d("calculate md5 [$digest]");
-                            $s->d("digest[$digest]\n");
-                            my $val = $cfg->val( $sec, "checksum" );
-                            push @{ $before{$sec} }, $val;
-                            push @{ $after{$sec} },  $digest;
+                        my $ctx = Digest::MD5->new;
+                        open my $f, q{<}, $fn or die "Can not read [$fn]!\n";
+                        $ctx->addfile($f);
+                        my $digest = $ctx->hexdigest;
+                        close $f;
+                        $s->d("    calculate md5 [$digest]");
+                        $s->d("    digest[$digest]\n");
+                        my $val = $cfg->val( $sec, "checksum" );
+                        push @{ $before{$sec} }, $val;
+                        push @{ $after{$sec} },  $digest;
 
-                            if ( $val ne $digest ) {
-                                $cfg->setval( $sec, "checksum", $digest );
-                                $s->d("write [$cfn]\n");
-                                $cfg->WriteConfig($cfn);
-                                $s->p("changed section [$sec]:\n");
-                                $s->p("    checksum=$digest\n");
-                                $s->p("wrote [$cfn]\n");
-                                $updated++;
-                            }
-                        }
-                        else {
-                            $s->e(
-                                "section [$sec] in $cfn has not checksum attribute",
-                                'cfg'
-                            );
+                        if ( $val ne $digest ) {
+                            $cfg->setval( $sec, "checksum", $digest );
+                            $s->d("write [$cfn]\n");
+                            $cfg->WriteConfig($cfn);
+                            $s->p("changed section [$sec]:\n");
+                            $s->p("    checksum=$digest\n");
+                            $s->p("wrote [$cfn]\n");
+                            $updated++;
                         }
                     }
                     else {
-                        $s->d("NG\n");
+                        $s->d("    $ng src[$src]");
+                        $s->d("    $ng fn [$fn]\n");
                     }
                 }
             }
